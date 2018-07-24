8fff8b3493716edd4dbc244a5868cf1f23c69977
josesaballo13@gmail.com <josesaballo13@gmail.com>
2018-07-23 22:29:27 -0500

    splash screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,23 +4,25 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
-import { SplashPage } from '../pages/splash/splash';
-import { ModalController } from 'ionic-angular/components/modal/modal-controller';
+
+import { timer } from 'rxjs/Observable/timer';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+  
   rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, modalCtrl: ModalController) {
+  showSplash = true;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
-      let splash = modalCtrl.create(SplashPage);
-      splash.present();
-      //splashScreen.hide();
+      splashScreen.hide();
+      timer(3000).subscribe(() => this.showSplash = false);
     });
   }
 }
